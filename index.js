dcf76a48520d15112414e171a1acb34b2f4b4e4b
LAXMIKHANT <your_github_naiklaxmikanth0@gmail.com>
2026-03-04 09:59:48 +0530

    add new

---
+++ index.js
@@ -0,0 +1,15 @@
+// let num = document.getElementsByClassName("num");
+// let counting = document.getElementsByClassName("buttonbox");
+
+
+// let count =0;
+//     updateDisplay();
+// counting[0].addEventListener("click",()=>{
+//     count++;
+//         updateDisplay();
+// });
+
+// function updateDisplay(){
+//     num[0].innerHTML=count;
+
+// };
---
+++ user.js
@@ -0,0 +1,18 @@
+let username;
+
+document.getElementById("submit").onclick = function (){
+    let username =document.getElementById("inputype").value
+    console.log (username);
+};
+
+
+
+
+
+
+
+
+
+
+
+
